41cc428cad5f2423bc8adfcb11fd5031e2ee2aba
obarboza92 <orlando.barboza92@gmail.com>
2024-04-22 23:03:37 -0600

    actualizacion de mensaje de carga de imagen

--- background_remover.py
+++ background_remover.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.title("Background Remover Demo")
 col1, col2 = st.columns(2)
-images = st.sidebar.file_uploader("Cargue la imagen deseada", accept_multiple_files =True)
+images = st.sidebar.file_uploader("Carga la imagen deseada", accept_multiple_files =True)
 if images:
     for image in images:
         with Image.open(image) as img:
@@ -14,4 +14,6 @@ if images:
             output = remove(img)
             col2.header("Extracted")
             col2.image(output)
-            
+
+
+        
